dceb1bbf2201a1cc9ead08d102cd9b1b4864891c
Likegenm <Oleg.25478@mail.ru>
2025-11-10 18:25:58 +0300

    Update ArmyRp.lua

--- ArmyRp.lua
+++ ArmyRp.lua
@@ -1,7 +1,3 @@
 --LIKEGENM
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP2.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP3.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP4.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP5.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP6.lua"))()
