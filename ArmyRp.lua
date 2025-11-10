3bf30b81876567cee458a2622436554c718f6a5f
Likegenm <Oleg.25478@mail.ru>
2025-11-10 18:00:17 +0300

    Create ArmyRp.lua

---
+++ ArmyRp.lua
@@ -0,0 +1,7 @@
+--LIKEGENM
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP3.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP4.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP5.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRP6.lua"))()
