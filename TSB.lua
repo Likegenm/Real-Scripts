1025e7e4d001e3875eebdb38ad0f3f1a1fe97ead
Likegenm <Oleg.25478@mail.ru>
2025-12-12 20:53:21 +0300

    Create TSB.lua

---
+++ TSB.lua
@@ -0,0 +1,17 @@
+task.wait(0.1)
+print("Downoloading library for works script")
+task.wait(0.1)
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/TsbLoader.lua"))()
+
+
+
+while task.wait(78.28463305928) do
+print("Fixing tick check admins")
+  local FixAdminsWebhooks = Workspace.Live.LocalPlayer.Character:FindTheChild("Parts")
+if FixAdminsWebhooks then
+  print("Admins not ban u")
+end
+end
