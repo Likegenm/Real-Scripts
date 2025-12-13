6a35d041e5f783b8138c464108326294399457f7
Likegenm <Oleg.25478@mail.ru>
2025-12-13 14:33:23 +0300

    Update TSB.lua

--- TSB.lua
+++ TSB.lua
@@ -5,13 +5,3 @@ task.wait(0.1)
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/TsbLoader.lua"))()
-
-
-
-while task.wait(78.28463305928) do
-print("AntiBan check")
-  local FixAdminsWebhooks = game.Players.LocalPlayer.Character:FindFirstChild("Pants")
-if FixAdminsWebhooks then
-  print("Admins not ban u")
-end
-end
