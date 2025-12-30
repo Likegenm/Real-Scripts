dfe033dcd7b595a6384e684e9a66cdad2e2e9fe5
Likegenm <Oleg.25478@mail.ru>
2025-12-30 16:02:52 +0300

    Create The Field + Dex.lua

---
+++ The Field + Dex.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/ForCreate/refs/heads/main/Dexv6.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/The%20Field.lua"))()
