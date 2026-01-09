18cc01ebdfff96fc528d7d25a995df078a572fed
Likegenm <Oleg.25478@mail.ru>
2026-01-09 19:00:58 +0300

    Update Auto.lua

--- Auto.lua
+++ Auto.lua
@@ -1,37 +1,31 @@
-local id = game.gameId
-
-local function load(url)
-  loadstring(game:HttpGet(url, true))()
+if game.gameId == 3808081382 then
+    loadstring(game:HttpGet("https://github.com/Likegenm/Real-Scripts/blob/main/TSB.lua", true))()
 end
 
-if id = 3808081382 then
-  load("https://github.com/Likegenm/Real-Scripts/blob/main/TSB.lua")
+if game.gameId == 3109731140 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/Intruder.lua", true))()
 end
 
-if id = 3109731140 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/Intruder.lua")
+if game.gameId == 66654135 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/MM2.lua", true))()
 end
 
-if id = 66654135 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/MM2.lua")
+if game.gameId == 1742264997 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ScpRP.lua", true))()
 end
 
-if id = 1742264997 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ScpRP.lua")
+if game.gameId == 99402433 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/Speedrun12.lua", true))()
 end
 
-if id = 99402433 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/Speedrun12.lua")
+if game.gameId == 7709344486 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/SB(Steal%20A%20Brainrot.lua", true))()
 end
 
-if id = 7709344486 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/SB(Steal%20A%20Brainrot.lua")
+if game.gameId == 6699967032 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/The%20Field.lua", true))()
 end
 
-if id = 6699967032 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/The%20Field.lua")
-end
-
-if id = 6280758286 then
-  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRp.lua")
+if game.gameId == 6280758286 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ArmyRp.lua", true))()
 end
