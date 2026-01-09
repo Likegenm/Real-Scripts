eeabe661440f2bf973203b8c7fe616735410184b
Likegenm <Oleg.25478@mail.ru>
2026-01-09 18:52:30 +0300

    Create Auto.lua

---
+++ Auto.lua
@@ -0,0 +1,21 @@
+local id = game.gameId
+
+local function load(url)
+  loadstring(game:HttpGet(url, true))()
+end
+
+if Id = 3808081382 then
+  load("https://github.com/Likegenm/Real-Scripts/blob/main/TSB.lua")
+end
+
+if Id = 3109731140 then
+  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/Intruder.lua")
+end
+
+if id = 66654135 then
+  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/MM2.lua")
+end
+
+if id = 1742264997 then
+  load("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/ScpRP.lua")
+end
