4d81408e4eb66877fbbb38b2c99ce5f81c2c6893
Likegenm <Oleg.25478@mail.ru>
2026-04-15 19:30:21 +0300

    Create Invisile.lua

---
+++ Invisile.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/BypassVoid.lua"))()
+
+task.wait(0.1)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/InvisforPhantasm.lua"))()
