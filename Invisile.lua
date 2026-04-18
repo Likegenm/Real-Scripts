0315c7b14dce97f46d913ea355d2a1fdcb04112c
Likegenm <Oleg.25478@mail.ru>
2026-04-18 12:11:27 +0300

    Update Invisile.lua

--- Invisile.lua
+++ Invisile.lua
@@ -1,5 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/BypassVoid.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/InvisforPhantasm.lua"))()
 
 task.wait(0.1)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/InvisforPhantasm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/BypassVoid.lua"))()
